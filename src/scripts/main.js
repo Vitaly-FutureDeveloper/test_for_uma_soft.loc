2e2639ac5f7fa136eea9f7d7b1ce4b5622e83675
Vitaliy-Future-Developer <zarvoron@yandex.ru>
2023-09-07 00:51:27 +0300

    add animation lib

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 import {favoriteSlider} from "./module/favorite-slider.js";
+import {SkrollAnimation} from "./module/scroll-animation.js";
 
+
+// Lib https://github.com/akzhy/skroll/tree/master
+SkrollAnimation.init();
+
+// Самописный слайдер - полностью адаптивный
 favoriteSlider('favorites-slider');
